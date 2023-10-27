66e848701bfbc7ed13e064cc4ffb33748d82ef34
Anoosh Moutafian <anooshmoutafian@gmail.com>
2023-10-27 16:23:09 -0700

    Update SQLTasks Tier 1.sql

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -72,6 +72,7 @@ Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
+SELECT * FROM members
 
 /* Q8: Produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30. Remember that guests have
